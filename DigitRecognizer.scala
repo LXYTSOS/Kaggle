e0676ef65538a078ac2f715f6f3c584c81403a50
LXYTSOS <834287447@qq.com>
2016-05-13 09:49:54 +0800

    随机森林
    使用随机森林进行分类，并参数调忧。

--- DigitRecognizer.scala
+++ DigitRecognizer.scala
@@ -5,6 +5,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.classification.NaiveBayes
+import org.apache.spark.mllib.tree.RandomForest
 
 /**
  * @author sl169
@@ -25,9 +26,9 @@ object DigitRecognizer {
       LabeledPoint(label, Vectors.dense(features))
     }
     
-    val nbModel = NaiveBayes.train(data)
+//    val nbModel = NaiveBayes.train(data)
     
-//     val nbTotalCorrect = data.map { point =>
+//    val nbTotalCorrect = data.map { point =>
 //      if (nbModel.predict(point.features) == point.label) 1 else 0
 //    }.sum
 //    val numData = data.count()
@@ -39,21 +40,67 @@ object DigitRecognizer {
     
     val unlabeledData = sc.textFile("file:///home/shdxspark/suisui/test-noheader.csv")
     val unlabeledRecords = unlabeledData.map(line => line.split(","))
-    val featrues = unlabeledRecords.map{ r =>
+    val features = unlabeledRecords.map{ r =>
       val f = r.map(p => p.toDouble)
       Vectors.dense(f)
     }
-    val num = unlabeledData.count()
+//    val num = unlabeledData.count()
     
-    val predictions = nbModel.predict(featrues).map { p => p.toInt }
-    val raw = unlabeledData.collect()
-    val pre = predictions.collect()
-    val p = (0 to num.toInt - 1).map{ i =>
-      val label = pre(i).toString()
-      val feature = raw(i)
-      label+","+feature
-    }
-    val result = sc.parallelize(p)
-    result.repartition(1).saveAsTextFile("file:///home/shdxspark/suisui/digitRec.txt")
+//    val predictions = nbModel.predict(features).map { p => p.toInt }
+//    val raw = unlabeledData.collect()
+//    val pre = predictions.collect()
+//    val p = (0 to num.toInt - 1).map{ i =>
+//      val label = pre(i).toString()
+//      val feature = raw(i)
+//      label+","+feature
+//    }
+//    val result = sc.parallelize(p)
+//    result.repartition(1).saveAsTextFile("file:///home/shdxspark/suisui/digitRec.txt")
+    
+    //随机森林模型
+    val numClasses = 10
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val numTrees = 70 
+    val featureSubsetStrategy = "auto" 
+    val impurity = "gini"
+    val maxDepth = 30
+    val maxBins = 32
+    val randomForestModel = RandomForest.trainClassifier(data, numClasses, categoricalFeaturesInfo, numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+    
+//    val nbTotalCorrect = data.map { point =>
+//      if (randomForestModel.predict(point.features) == point.label) 1 else 0
+//    }.sum
+//    val numData = data.count()
+//    println(numData)
+//    //42000
+//    val nbAccuracy = nbTotalCorrect / numData
+//    println("准确率："+nbAccuracy)
+    //numTree=3,maxDepth=4,准确率：0.5507619047619048
+    //numTree=4,maxDepth=5,准确率：0.7023095238095238
+    //numTree=5,maxDepth=6,准确率：0.693595238095238
+    //numTree=6,maxDepth=7,准确率：0.8426428571428571
+    //numTree=7,maxDepth=8,准确率：0.879452380952381
+    //numTree=8,maxDepth=9,准确率：0.9105714285714286
+    //numTree=9,maxDepth=10,准确率：0.9446428571428571
+    //numTree=10,maxDepth=11,准确率：0.9611428571428572
+    //numTree=11,maxDepth=12,准确率：0.9765952380952381
+    //numTree=12,maxDepth=13,准确率：0.9859523809523809
+    //numTree=13,maxDepth=14,准确率：0.9928333333333333
+    //numTree=14,maxDepth=15,准确率：0.9955
+    //numTree=15,maxDepth=16,准确率：0.9972857142857143
+    //numTree=16,maxDepth=17,准确率：0.9979285714285714
+    //numTree=17,maxDepth=18,准确率：0.9983809523809524
+    //numTree=18,maxDepth=19,准确率：0.9989285714285714
+    //numTree=19,maxDepth=20,准确率：0.9989523809523809
+    //numTree=20,maxDepth=21,准确率：0.999
+    //numTree=21,maxDepth=22,准确率：0.9994761904761905
+    //numTree=22,maxDepth=23,准确率：0.9994761904761905
+    //numTree=23,maxDepth=24,准确率：0.9997619047619047
+    //numTree=24,maxDepth=25,准确率：0.9997857142857143
+    //numTree=25,maxDepth=26,准确率：0.9998333333333334
+    //numTree=29,maxDepth=30,准确率：0.9999523809523809
+    
+    val predictions = randomForestModel.predict(features).map { p => p.toInt }
+    predictions.repartition(1).saveAsTextFile("file:///home/shdxspark/suisui/digitRec.txt")
   }
 }
